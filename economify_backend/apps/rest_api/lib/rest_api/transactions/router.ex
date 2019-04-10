85a0bd0822f6543227e2aec2739154bfedc34580
rogelio-o <yo@rogelioorts.com>
2019-04-10 21:52:47 +0200

    Refactorizes service and controller.

--- economify_backend/apps/rest_api/lib/rest_api/transactions/router.ex
+++ economify_backend/apps/rest_api/lib/rest_api/transactions/router.ex
@@ -5,73 +5,93 @@ defmodule RestApi.Transactions.Router do
   plug(:dispatch)
 
   get "/" do
-    page = conn.params["page"] || 1
-    page_size = conn.params["page_size"] || 5
+    page = get_page(conn)
+    page_size = get_page_size(conn, "5")
 
-    send_json(conn, 200, Transactions.Interface.get_all_paginated(page, page_size))
+    Transactions.Interface.get_all_paginated(page, page_size)
+    |> send_as_json(200, conn)
   end
 
   get "/:transaction_id" do
-    transaction_id = conn.path_params["transaction_id"]
-
-    send_json(conn, 200, Transactions.Interface.get_by_id(transaction_id))
+    conn.path_params["transaction_id"]
+    |> Transactions.Interface.get_by_id()
+    |> send_entity_as_json(conn)
   end
 
   post "/" do
-    {status, body} =
-      case conn.body_params do
-        %{"transactions" => transactions} -> {200, create_transactions(transactions)}
-        _ -> {422, missing_transactions()}
-      end
-
-    send_json(conn, status, body)
-  end
-
-  defp create_transactions(transactions) do
-    Enum.map(transactions, &create_transaction/1)
-  end
-
-  defp create_transaction(transaction) do
-    case Transactions.Interface.create(transaction) do
-      {:ok, created_transaction} ->
-        %{
-          transaction_id: created_transaction.transaction_id
-        }
-
-      {:error, errors} ->
-        errors
-    end
-  end
-
-  defp missing_transactions do
-    %{error: "Expected Payload: { 'transactions': [...] }"}
+    conn.body_params
+    |> Transactions.Interface.create()
+    |> send_create_result_as_json(conn)
   end
 
   put "/:transaction_id" do
-    {status, body} = update_transaction(conn.path_params["transaction_id"], conn.body_params)
-
-    send_json(conn, status, body)
-  end
-
-  defp update_transaction(transaction_id, params) do
-    case Transactions.Interface.update(transaction_id, params) do
-      {:ok, _} ->
-        {200, %{success: true}}
-
-      {:error, errors} ->
-        {400, errors}
-    end
+    conn.path_params["transaction_id"]
+    |> Transactions.Interface.update(conn.body_params)
+    |> send_update_result_as_json(conn)
   end
 
   delete "/:transaction_id" do
-    {:ok, _} = Transactions.Interface.delete(conn.path_params["transaction_id"])
-
-    send_json(conn, 200, %{success: true})
+    conn.path_params["transaction_id"]
+    |> Transactions.Interface.delete()
+    |> send_delete_result_as_json(conn)
   end
 
-  defp send_json(conn, status, body) do
+  defp send_entity_as_json(result, conn) do
+    case result do
+      {:not_found} ->
+        send_empty(404, conn)
+
+      {:bad_request} ->
+        send_empty(400, conn)
+
+      {:ok, entity} ->
+        send_as_json(entity, 200, conn)
+    end
+  end
+
+  defp send_result_as_json(result, ok_status, conn) do
+    case result do
+      {:ok, transaction} -> send_as_json(transaction, ok_status, conn)
+      {:form_error, errors} -> send_as_json(errors, 422, conn)
+      {:not_found} -> send_empty(404, conn)
+      {:bad_request} -> send_empty(400, conn)
+    end
+  end
+
+  defp send_create_result_as_json(result, conn) do
+    send_result_as_json(result, 201, conn)
+  end
+
+  defp send_update_result_as_json(result, conn) do
+    send_result_as_json(result, 200, conn)
+  end
+
+  defp send_delete_result_as_json(result, conn) do
+    case result do
+      {:ok, _} -> send_empty(204, conn)
+      {:not_found} -> send_empty(204, conn)
+      {:bad_request} -> send_empty(400, conn)
+    end
+  end
+
+  defp send_as_json(body, status, conn) do
     conn
     |> put_resp_content_type("application/json")
     |> send_resp(status, Poison.encode!(body))
+  end
+
+  defp send_empty(status, conn) do
+    conn
+    |> send_resp(status, "")
+  end
+
+  defp get_page(conn) do
+    (conn.params["page"] || "1")
+    |> String.to_integer()
+  end
+
+  defp get_page_size(conn, default) do
+    (conn.params["page_size"] || default)
+    |> String.to_integer()
   end
 end
--- economify_backend/apps/transactions/lib/transactions/service.ex
+++ economify_backend/apps/transactions/lib/transactions/service.ex
@@ -2,56 +2,62 @@ defmodule Transactions.Service do
   require Ecto.Query
 
   def create(params) do
-    transaction = %Transactions.Schema{}
-    changeset = Transactions.Schema.changeset(transaction, params)
-
-    case Transactions.Repo.insert(changeset) do
-      {:ok, created_transaction} ->
-        {:ok, created_transaction}
-
-      {:error, changeset} ->
-        {:error, translate_errors(changeset)}
-    end
+    %Transactions.Schema{}
+    |> Transactions.Schema.changeset(params)
+    |> Transactions.Repo.insert()
+    |> parse_result
   end
 
   def update(transaction_id, params) do
-    transaction = get_by_id(transaction_id)
-    changeset = Transactions.Schema.changeset(transaction, params)
-
-    case Transactions.Repo.update(changeset) do
-      {:ok, updated_transaction} ->
-        {:ok, updated_transaction}
-
-      {:error, changeset} ->
-        {:error, translate_errors(changeset)}
+    try do
+      %Transactions.Schema{transaction_id: transaction_id}
+      |> Transactions.Schema.changeset(params)
+      |> Transactions.Repo.update()
+      |> parse_result
+    rescue
+      _e in Ecto.StaleEntryError -> {:not_found}
+      _e in Ecto.ChangeError -> {:bad_request}
     end
   end
 
   def delete(transaction_id) do
-    transaction = get_by_id(transaction_id)
-    Transactions.Repo.delete(transaction)
+    case get_by_id(transaction_id) do
+      {:not_found} -> {:not_found}
+      {:bad_request} -> {:bad_request}
+      {:ok, transaction} -> Transactions.Repo.delete(transaction)
+    end
   end
 
   def get_all_paginated(page, page_size) do
-    page =
-      Ecto.Query.from(p in Transactions.Schema, order_by: [desc: p.date])
-      |> Transactions.Repo.paginate(page: page, page_size: page_size)
-
-    %{
-      transactions: page.entries,
-      page_number: page.page_number,
-      page_size: page.page_size,
-      total_pages: page.total_pages,
-      total_entries: page.total_entries
-    }
+    Ecto.Query.from(p in Transactions.Schema, order_by: [desc: p.date])
+    |> Transactions.Repo.paginate(page: page, page_size: page_size)
   end
 
   def get_by_id(transaction_id) do
-    Transactions.Schema |> Transactions.Repo.get(transaction_id)
+    try do
+      result = Transactions.Schema |> Transactions.Repo.get(transaction_id)
+
+      case result do
+        nil -> {:not_found}
+        _ -> {:ok, result}
+      end
+    rescue
+      _e in Ecto.Query.CastError -> {:bad_request}
+    end
   end
 
   defp translate_errors(changeset) do
     Ecto.Changeset.traverse_errors(changeset, &translate_error/1)
+  end
+
+  defp parse_result(result) do
+    case result do
+      {:ok, transaction} ->
+        {:ok, transaction}
+
+      {:error, changeset} ->
+        {:form_error, translate_errors(changeset)}
+    end
   end
 
   defp translate_error({msg, opts}) do
