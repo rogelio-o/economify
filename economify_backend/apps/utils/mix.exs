482cae731fbe706a6029ddf17925c44cd4da81d9
rogelio-o <yo@rogelioorts.com>
2019-04-16 18:48:55 +0200

    Extracts utils to a separate project.

--- economify_backend/apps/utils/mix.exs
+++ economify_backend/apps/utils/mix.exs
@@ -1,9 +1,9 @@
-defmodule Transactions.MixProject do
+defmodule Utils.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :transactions,
+      app: :utils,
       version: "0.1.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
@@ -15,21 +15,17 @@ defmodule Transactions.MixProject do
     ]
   end
 
+  # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
-      mod: {Transactions.Application, []}
+      extra_applications: [:logger]
     ]
   end
 
+  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:poison, "~> 3.1"},
-      {:ecto_sql, "~> 3.0"},
-      {:postgrex, ">= 0.0.0"},
-      {:scrivener_ecto, "~> 2.2.0"},
-      {:ecto_enum, "~> 1.2"},
-      {:utils, in_umbrella: true}
+      {:ecto_sql, "~> 3.0"}
     ]
   end
 end
