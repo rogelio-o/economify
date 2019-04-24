4320f68bfa4e68a793df387a6495086e9c0a24d2
rogelio-o <yo@rogelioorts.com>
2019-04-24 20:23:14 +0200

    Shorts pagination.

--- economify_frontend/src/components/DataPagination.js
+++ economify_frontend/src/components/DataPagination.js
@@ -6,6 +6,32 @@ const handleClick = (e, loadPage, page) => {
   loadPage(page);
 };
 
+const calculatePages = (totalPages, currentPage) => {
+  const pages = [];
+
+  const minPage = Math.max(currentPage - 5, 1);
+  if (minPage > 1) {
+    pages.push(1);
+    pages.push('...');
+  }
+
+  for (let i = minPage; i < currentPage; i++) {
+    pages.push(i);
+  }
+
+  const maxPage = Math.min(totalPages, currentPage + 5);
+  for (let i = currentPage; i <= maxPage; i++) {
+    pages.push(i);
+  }
+
+  if (maxPage !== totalPages) {
+    pages.push('...');
+    pages.push(totalPages);
+  }
+
+  return pages;
+};
+
 const DataPagination = ({ data, loadPage }) => {
   const currentPage = data.page_number;
   const totalPages = data.total_pages;
@@ -13,6 +39,11 @@ const DataPagination = ({ data, loadPage }) => {
   if (totalPages === 1) {
     return null;
   }
+
+  const pages =
+    totalPages <= 10
+      ? [...Array(totalPages).keys()]
+      : calculatePages(totalPages, currentPage);
 
   return (
     <Pagination size="md">
@@ -25,14 +56,18 @@ const DataPagination = ({ data, loadPage }) => {
           />
         </PaginationItem>
       ) : null}
-      {[...Array(totalPages).keys()].map(page => (
-        <PaginationItem key={`page-${page}`} active={currentPage === page + 1}>
-          <PaginationLink
-            href="#"
-            onClick={e => handleClick(e, loadPage, page + 1)}
-          >
-            {page + 1}
-          </PaginationLink>
+      {pages.map(page => (
+        <PaginationItem key={`page-${page}`} active={currentPage === page}>
+          {page === '...' ? (
+            <PaginationLink ellipsis="...">...</PaginationLink>
+          ) : (
+            <PaginationLink
+              href="#"
+              onClick={e => handleClick(e, loadPage, page)}
+            >
+              {page}
+            </PaginationLink>
+          )}
         </PaginationItem>
       ))}
       {currentPage < totalPages ? (
